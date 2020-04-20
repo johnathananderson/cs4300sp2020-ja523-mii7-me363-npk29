7cb6dda4ace0bd63874fe0c69475ad366ea14493
Nakia Kenon <nakiakenon@Nakias-MacBook-Pro.local>
2020-04-20 19:38:55 -0400

    I really hope this doesnt break the app

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,7 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from findation import *
 
 project_name = "Save Face"
 net_id = "ja523, me363, mii7, npk29"
@@ -8,12 +9,14 @@ net_id = "ja523, me363, mii7, npk29"
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
-	if not query:
+	if not query or len(query)==0:
 		data = []
 		output_message = "Its not working"
 	else:
 		output_message = "Your search: " + query
-		data = query
+		query = str(query)
+		data = process_matches(query.split())
+
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
