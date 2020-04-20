c579e206ff4f98c8e7fc7b2f17595de6991a8503
Marcella Imoisili <mii7@cornell.edu>
2020-04-20 18:05:09 -0400

    Testing controller

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -13,7 +13,7 @@ def search():
 		output_message = "Its not working"
 	else:
 		output_message = "Your search: " + query
-		data = range(5)
+		data = query
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
