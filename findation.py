8e6a5dbe6046a882b5363d88168bdbf41e35693e
johnathananderson <andersonjohnathan@yahoo.com>
2020-04-20 23:33:55 -0400

    Bug fix

--- findation.py
+++ findation.py
@@ -9,9 +9,9 @@ from ingredients import IngredientsBrowser
 class FindationBrowser:
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("headless")
-        # chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--no-sandbox")
         chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_SHIM", None)
         self.browser = webdriver.Chrome(chrome_options=chrome_options, executable_path="chromedriver")
         self.browser.implicitly_wait(3)
