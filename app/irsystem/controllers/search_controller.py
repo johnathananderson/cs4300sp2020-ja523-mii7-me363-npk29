f8cbcd26da997662b4809d576618fe274e047d54
Nakia Kenon <nakiakenon@Nakias-MacBook-Pro.local>
2020-04-20 20:13:10 -0400

    changes

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,7 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from findation import *
 
 project_name = "Save Face"
 net_id = "ja523, me363, mii7, npk29"
@@ -13,7 +14,8 @@ def search():
 		output_message = "Its not working"
 	else:
 		output_message = "Your search: " + query
-		data = range(5)
+		query = query.split()
+		data = process_matches(self, query)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
