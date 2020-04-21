ff3c41e588011b03ff3c7093fed87efdd6c47aab
johnathananderson <andersonjohnathan@yahoo.com>
2020-04-20 20:09:08 -0400

    Adding ingredient scraping

---
+++ ingredients.py
@@ -0,0 +1,64 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class IngredientsBrowser:
+    def __init__(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("headless")
+        # chrome_options.add_experimental_option("prefs", {"profile.default_content_settings.cookies": 2})
+        self.browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+
+    def close_out(self):
+        self.browser.close()
+
+    def find_ingredients(self, brand, product):
+        base_url = "https://www.ewg.org/skindeep/search/?utf8=%E2%9C%93&search="
+        self.browser.get(base_url + brand + "+" + product)
+        time.sleep(1)
+        found = False
+        while not found:
+            try:
+                search_header = self.browser.find_element_by_class_name("browse-search-header")
+                found = True
+            except:
+                time.sleep(1)
+                print(1)
+        if search_header.text[0] == "0":
+            base_url = "https://www.beautypedia.com/?s="
+            self.browser.get(base_url + brand + "+" + product)
+            time.sleep(1)
+            found = False
+            while not found:
+                try:
+                    search_header = self.browser.find_element_by_class_name("search-results-summary")
+                    found = True
+                except:
+                    time.sleep(1)
+            if search_header.text[0] == "0":
+                return "Ingredients not found"
+            else:
+                review_results = self.browser.find_element_by_class_name("review-results")
+                self.browser.get(review_results.find_element_by_class_name("review-product").get_attribute("href"))
+                time.sleep(1)
+                try:
+                    i = self.browser.find_element_by_xpath("//*[@id='ingredients']")
+                    ingredients = (
+                        i.get_attribute("innerHTML")
+                        .replace("Active: ", "")
+                        .replace("Other: ", "")
+                        .replace("May Contain: ", "")
+                        .replace("May contain: ", "")
+                        .strip()
+                        .split(",  ")
+                    )
+                    return ingredients
+                except:
+                    return "Ingredients not found"
+        else:
+            listings = self.browser.find_element_by_class_name("product-listings")
+            self.browser.get(listings.find_element_by_tag_name("a").get_attribute("href"))
+            time.sleep(1)
+            ingredients = self.browser.find_elements_by_class_name("td-ingredient-interior")
+            return list(map(lambda x: x.text.split("\n")[0].title(), ingredients))
