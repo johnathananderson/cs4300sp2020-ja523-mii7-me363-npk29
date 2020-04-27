bb26ff5b123314efddbf728ceab506efc4e5edd9
johnathananderson <andersonjohnathan@yahoo.com>
2020-04-27 18:17:09 -0400

    Test

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -13,29 +13,6 @@ net_id = "ja523, me363, mii7, npk29"
 
 @irsystem.route("/", methods=["GET"])
 def search():
-    # query = request.args.get("search")
-    # if not query:
-    #     data = []
-    #     output_message = "Its not working"
-    # else:
-    #     data = range(5)
-    #     output_message = "Your search: " + query
-    # # query = query.split()
-    # # brand1 = "Mary Kay"
-    # # product1 = "Full-Coverage Foundation"
-    # # shade1 = "Bronze 507"
-
-    # # brand2 = "Mary Kay"
-    # # product2 = "Medium-Coverage Foundation "
-    # # shade2 = "Bronze 507 (Natural)"
-
-    # # products = [[brand1, product1, shade1], [brand2, product2, shade2]]
-    # # f = FindationBrowser()
-    # # f.start()
-    # # time.sleep(1)
-    # # data = f.process_matches(products)
-    # # f.close_out()
-    # return render_template("search.html", name=project_name, netid=net_id, output_message=output_message, data=data)
     query = request.args.get("search")
     if not query:
         data = []
@@ -64,8 +41,24 @@ def product():
         output_message = "Your search: " + query
 
     return render_template("product.html", name=project_name, netid=net_id, output_message=output_message, data=data)
-    # return (request.form['search'])
 
+@irsystem.route("/product/test/", methods=["GET"])
+def product_test():
+    brand1 = "Mary Kay"
+    product1 = "Full-Coverage Foundation"
+    shade1 = "Bronze 507"
+
+    brand2 = "Mary Kay"
+    product2 = "Medium-Coverage Foundation "
+    shade2 = "Bronze 507 (Natural)"
+
+    products = [[brand1, product1, shade1], [brand2, product2, shade2]]
+    f = FindationBrowser()
+    f.start()
+    data = f.process_matches(products)
+    print(data)
+    f.close_out()
+    return render_template("product.html", name=project_name, netid=net_id, output_message=output_message, data=data)
 
 @irsystem.route("/outputs/", methods=["GET"])
 def outputs():
