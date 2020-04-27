27daa9031a9948868a824b868c49ae59c0171887
Nakia Kenon <nakiakenon@Nakias-MacBook-Pro.local>
2020-04-27 19:08:24 -0400

    second app

---
+++ app-two/irsystem/controllers/search_controller.py
@@ -0,0 +1,20 @@
+from . import *  
+from app.irsystem.models.helpers import *
+from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+
+project_name = "Save Face"
+net_id = "ja523, me363, mii7, npk29"
+
+@irsystem.route('/', methods=['GET'])
+def search():
+	query = request.args.get('search')
+	if not query:
+		data = []
+		output_message = ''
+	else:
+		output_message = "Your search: " + query
+		data = query
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+
+
+
