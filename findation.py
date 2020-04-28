284175cd0e52a06b3d6e03520edb19e73cfc0c8c
johnathananderson <andersonjohnathan@yahoo.com>
2020-04-28 19:41:35 -0400

    Test

--- findation.py
+++ findation.py
@@ -13,7 +13,7 @@ class FindationBrowser:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_argument("--proxy-server='direct://'")
         chrome_options.add_argument("--proxy-bypass-list=*")
