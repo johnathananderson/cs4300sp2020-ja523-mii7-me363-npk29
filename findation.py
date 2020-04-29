9e9ad2703cefb969d5abe0b55cccf0b921ec288e
johnathananderson <andersonjohnathan@yahoo.com>
2020-04-29 18:52:34 -0400

    Test

--- findation.py
+++ findation.py
@@ -43,7 +43,6 @@ class FindationBrowser:
                 print("Couldn't open outputs")
                 outputs = {}
         try:
-            print(self.browser.find_element_by_id("brand-search").value_of_css_property("height"))
             WebDriverWait(self.browser, 20).until(
                 EC.visibility_of_element_located((By.XPATH, "//*[@id='hide-splash']"))
             )
