3835d123ff1e6bb6ed5985f551986df25ed2c74b
johnathananderson <andersonjohnathan@yahoo.com>
2020-04-29 04:56:42 -0400

    Test

--- findation.py
+++ findation.py
@@ -16,7 +16,7 @@ class FindationBrowser:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_argument("--no-proxy-server")
         chrome_options.add_argument("--proxy-server='direct://'")
@@ -64,8 +64,10 @@ class FindationBrowser:
                 product_name = product[1]
                 shade = product[2]
                 print(10)
-                while self.browser.find_element_by_id("brand-search").value_of_css_property("width") == "100%":
-                    print(self.browser.find_element_by_id("brand-search").value_of_css_property("width"))
+                while (
+                    self.browser.find_element_by_class_name("search-input").value_of_css_property("display") == "none"
+                ):
+                    print(self.browser.find_element_by_class_name("search-input").value_of_css_property("display"))
                     time.sleep(0.5)
                 brand_input = WebDriverWait(self.browser, 20, 0.1).until(
                     EC.visibility_of_element_located((By.ID, "brand-search"))
