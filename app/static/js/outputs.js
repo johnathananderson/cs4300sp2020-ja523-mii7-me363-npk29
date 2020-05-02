44ffbc4f014a6a8b65072c0f9d6d8ded1cf0c2e8
johnathananderson <andersonjohnathan@yahoo.com>
2020-05-02 16:14:26 -0400

    Changing outputs page

--- app/static/js/outputs.js
+++ app/static/js/outputs.js
@@ -4,18 +4,8 @@ $(document).ready(function () {
 
     $(".filter-button").click(function () {
         var value = $(this).attr('data-filter');
-
-        if (value == "Alphabetical") {
-            //$('.filter').removeClass('hidden');
-            $('.filter').show('1000');
-            console.log(alphabetical);
-        } else {
-            //            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-            //            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
-            $(".filter").not('.' + value).hide('3000');
-            $('.filter').filter('.' + value).show('3000');
-
-        }
+        $('.filter').filter('.' + value).show('3000');
+        $('.filter').not('.' + value).hide('3000')
     });
 
     if ($(".filter-button").removeClass("active")) {
