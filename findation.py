58a896b8d5d7367b659c2d5dde8ed2ed23664a9e
johnathananderson <andersonjohnathan@yahoo.com>
2020-05-02 02:25:15 -0400

    Changing outputs page

--- findation.py
+++ findation.py
@@ -137,7 +137,11 @@ class FindationBrowser:
                         match_product["brand"] = match_brand
                         match_product["name"] = match_name
                         match_product["shade"] = match_shade
-                        match_product["thumbnail"] = match.find_element_by_class_name("micro").get_attribute("src")
+                        match_product["thumbnail"] = (
+                            match.find_element_by_class_name("micro")
+                            .get_attribute("src")
+                            .replace("micro_thumb", "medium")
+                        )
                         # match_product["url"] = match.find_element_by_class_name("media").get_attribute("href")
 
                         if match_brand in outputs and match_name in outputs[match_brand]:
