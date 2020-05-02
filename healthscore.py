96828d35c85dffb8a67b98ef495b0b2192ddfb3a
Moses Eboh <me363@cornell.edu>
2020-05-01 21:42:39 -0500

    added brands to healthscores

--- healthscore.py
+++ healthscore.py
@@ -23,6 +23,7 @@ for cat in data:
         
 # Calculate score for each product
 for brand in product_ingredient_map:
+    final_scores[brand] = {}
     for product in product_ingredient_map[brand]:
         total_score = 0
         number_scores = 0
@@ -39,12 +40,12 @@ for brand in product_ingredient_map:
                 number_scores += 1
         if (number_scores == 0):
             # ewg did not score this product
-            final_scores[product] = 0
+            final_scores[brand][product] = 0
         else:
             # Scores were out of 10, 10 being worst. 
-            final_scores[product] = 10 - (total_score / (number_scores))
+            final_scores[brand][product] = 10 - (total_score / (number_scores))
         # Apply bonus weight for appearing in categories on paulaschoice.com
-        final_scores[product] += (PAULA_WEIGHT * health_count) 
+        final_scores[brand][product] += (PAULA_WEIGHT * health_count) 
     
 write_json(final_scores)
     
