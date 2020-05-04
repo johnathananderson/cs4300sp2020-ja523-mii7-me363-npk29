007a930eb56acda64559a5535ed78b3d3b9a1ace
johnathananderson <andersonjohnathan@yahoo.com>
2020-05-03 21:56:20 -0400

    Adding keyword filter

--- app/static/js/outputs.js
+++ app/static/js/outputs.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
     $('.filter:visible').not('.default').hide();
+    var filter = "default";
 
     $(".filter-button").click(function () {
-        var value = $(this).attr('data-filter');
-        $('.filter:hidden').filter('.' + value).show('3000');
-        $('.filter:visible').not('.' + value).hide('3000')
+        filter = $(this).attr('data-filter');
+        $('.' + filter).show('3000');
+        $('.filter:visible').not('.' + filter).hide('3000');
     });
 
     if ($(".filter-button").removeClass("active")) {
         $(this).removeClass("active");
     }
     $(this).addClass("active");
+
+    $("#brand-input").input(function () {
+        var value = $(this).attr('value');
+        $('.' + filter + ":hidden").filter(':contains(' + value + ')').show('3000');
+        $('.' + filter + ":visible").filter(':not(:contains(' + value + '))').hide('3000');
+    });
 
 });
