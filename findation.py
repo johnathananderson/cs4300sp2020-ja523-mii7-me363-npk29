44933f3babd7ada023fa3e798a8705f83875fff8
johnathananderson <andersonjohnathan@yahoo.com>
2020-05-04 16:17:21 -0400

    Optimizing findation script

--- findation.py
+++ findation.py
@@ -18,7 +18,7 @@ class FindationBrowser:
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-gpu")
+        # chrome_options.add_argument("--disable-gpu")
         # chrome_options.add_argument("--headless")
         chrome_options.add_argument("--log-level=3")
         chrome_options.add_argument("--no-proxy-server")
